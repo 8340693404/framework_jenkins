e4b8570906c4e87ad75a3b1cf0025ad5fe570cf1
yuvi <yuvi123@gmail.com>
2025-03-27 14:49:55 +0530

    jenkins

---
+++ SeleniumProject/src/test/java/pac2/orgTest.java
@@ -0,0 +1,18 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class orgTest {
+	@Test
+	public void createorgTest()
+	{
+		System.out.println("execute ");
+	}
+	@Test
+	public void modifyorgTest()
+	{
+		System.out.println("modify ");
+	}
+	
+
+}
